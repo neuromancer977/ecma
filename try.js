503c5420bd526c6d11ea510951d7428918999477
neuromancer977 <neuromancer977@gmail.com>
2016-10-24 11:25:27 +0200

    - (reverted from commit 50f5cae111140e306ad34eafbf08f2330750bd60)

--- try.js
+++ try.js
@@ -1,34 +1,6 @@
-var wu = require("wu");
-
 var arr = [1,2,3,4];
 console.log(arr);
-
-// array filtering
-console.log('// array filtering');
 var arr2 = arr.filter((item) => {return (item%2 === 0)});
+
 console.log(arr2);
-
-// find item
-console.log('// find item');
-var elem = arr.find((item) => {return item === 2})
-console.log(elem);
-
-// fibonacci filtering
-console.log('// fibonacci filtering');
-// fibonacci generator
-function* fibs() {
-  let a = 0;
-  let b = 1;
-  while (true) {
-    yield a;
-    [a, b] = [b, a + b];
-  }
-}
-
-const isEven      = n => n % 2 == 0;
-const lessThan10000 = n => n < 10000;
-
-wu(fibs())
-  .filter(isEven)
-  .takeWhile(lessThan10000)
-  .forEach(console.log.bind(console));
+console.log(arr2.find((item) => {return item === 2}));
